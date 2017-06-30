45711fcf63945a62539c5e07a08355130436e91e
Alex E Scott <aescott@outlook.com>
2017-06-29 17:06:20 -0700

    more about prototypes

--- es6/app/index.js
+++ es6/app/index.js
@@ -1,15 +1,17 @@
-import Animal from './animal.js';
+function Wizard(name, house, pet) {
+  this.name = name;
+  this.house = house;
+  this.pet = pet;
 
-class Lion extends Animal {
-  constructor(name, height, color) {
-    super(name, height);
-    this.color = color;
-  }
-
-  hello() {
-    console.log(`Hi, I'm ${this.name} from pride rock`);
-  }
+  this.greet = () => `I'm ${this.name}`;
 }
 
-let son = new Lion("Simba", 2, "gold");
-console.log(son.hello());
+Wizard.prototype.pet_name;
+
+Wizard.prototype.info = function() {
+  return `I have a ${this.pet} named ${this.pet_name}`;
+};
+
+let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
+harry.pet_name = "Hedwig";
+console.log(harry.info());
