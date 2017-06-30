47ffe03886e4b692fd52c9c9cfdf53bdd55dc1f8
Alex E Scott <aescott@outlook.com>
2017-06-29 17:50:56 -0700

    map and set in js

--- es6/app/index.js
+++ es6/app/index.js
@@ -1,17 +1,13 @@
-function Wizard(name, house, pet) {
-  this.name = name;
-  this.house = house;
-  this.pet = pet;
+let string = 'woefmeogneidkwodkgheo';
 
-  this.greet = () => `I'm ${this.name}`;
+let letters = new Map();
+for (let i=0; i<string.length; i++) {
+  let letter = string[i];
+  if (!letters.has(letter)) {
+    letters.set(letter, 1);
+  } else {
+    letters.set(letter, letters.get(letter) + 1);
+  }
 }
 
-Wizard.prototype.pet_name;
-
-Wizard.prototype.info = function() {
-  return `I have a ${this.pet} named ${this.pet_name}`;
-};
-
-let harry = new Wizard("Harry Potter", "Gryffindor", "Owl");
-harry.pet_name = "Hedwig";
-console.log(harry.info());
+console.log(letters);
