2a1a9dbb085b36d34038415abb7eebc2bbe11835
Alex E Scott <aescott@outlook.com>
2017-07-12 12:48:44 -0700

    add search bar to query google books

--- es6/app/Components/global.js
+++ es6/app/Components/global.js
@@ -3,17 +3,37 @@ import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 
 class Global extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    };
+  }
+
+  search() {
+    const BASE_URL = 'https://www.googleapis.com/books/v1/volumes?q=';
+    fetch(`${BASE_URL}${this.state.query}`, { method: 'GET' } )
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }
+
   render() {
     return (
-      <div>
+      <div className="Global">
         <h2>Book Explorer</h2>
         <FormGroup>
           <InputGroup>
             <FormControl
               type="text"
               placeholder="Search for a book"
+              onChange={event => this.setState({query: event.target.value})}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }
+              }}
               />
-            <InputGroup.Addon>
+            <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
           </InputGroup>
