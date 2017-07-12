cb9d07675edd5dd5d41b50d30770e6f663614306
Alex E Scott <aescott@outlook.com>
2017-07-12 11:25:15 -0700

    add awaitasync tab

---
+++ es6/app/awaitasync.js
@@ -0,0 +1,23 @@
+async function async_one() {
+  return "one";
+}
+
+async function async_two() {
+  return setTimeout(() => "two", 6000);
+}
+
+// async function async_three() {
+//   const one = await async_one();
+//   console.log(one);
+//   const two = await async_two();
+//   console.log(two);
+// }
+
+async function async_four() {
+  const [res_one, res_two] = await Promise.all(
+    [async_one(), async_two()]
+  )
+  console.log(res_one, res_two)
+}
+
+async_four();
--- es6/app/index.js
+++
@@ -1,5 +0,0 @@
-const root = 'https://www.reddit.com/user/Kijafa/submitted.json';
-
-fetch(root, { method: "GET"})
-  .then(response => response.json())
-  .then(json => console.log(json.data.children));
