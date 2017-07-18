2cfdef014a2c8bcaa875ca3b5891c9ae9ec312e8
Alex E Scott <aescott@outlook.com>
2017-07-17 18:21:29 -0700

    implement twoway binding

--- Angular/my-first-app/src/app/servers/servers.component.ts
+++ Angular/my-first-app/src/app/servers/servers.component.ts
@@ -2,14 +2,29 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: '<app-server></app-server><app-server></app-server>',
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  serverCreationStatus = 'No server was created!'
+  allowNewServer = false;
+  serverName = 'Testserver';
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
   }
 
 }
