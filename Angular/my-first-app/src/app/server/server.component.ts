18a9e561cc718600c44be701d487790bc25f23b5
Alex E Scott <aescott@outlook.com>
2017-07-20 17:57:29 -0700

    add type properties to server component

--- Angular/my-first-app/src/app/server/server.component.ts
+++ Angular/my-first-app/src/app/server/server.component.ts
@@ -6,8 +6,8 @@ import { Component } from '@angular/core';
 })
 
 export class ServerComponent {
-  serverID = 10;
-  serverStatus = 'offline';
+  serverID: number = 10;
+  serverStatus: string = 'offline';
 
   getServerStatus() {
     return this.serverStatus;
