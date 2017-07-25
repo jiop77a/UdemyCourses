7091153b920c6d532451e7924581799ad46c7ed7
Alex E Scott <aescott@outlook.com>
2017-07-25 15:26:59 -0700

    add stying to buttons

--- Angular/my-first-app/src/app/server/server.component.ts
+++ Angular/my-first-app/src/app/server/server.component.ts
@@ -3,11 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles: [`
-    .online {
-      color: white;
-    }
-    `]
+  styleUrls: ['./server.component.css']
+
 })
 
 export class ServerComponent {
