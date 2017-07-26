479812048c5533bf01d67c00875ca4253b6261e6
Alex E Scott <aescott@outlook.com>
2017-07-26 16:51:00 -0700

    add recipe list component

--- Angular/recipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular/recipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://s-media-cache-ak0.pinimg.com/736x/04/e0/52/04e0524d891999c1c435c1b1a707da5e--breakfast-photography-food-photography-styling.jpg')
+  ];
 
   constructor() { }
 
