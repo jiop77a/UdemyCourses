bdc3cf55829c2ada648d58a4eed441616cd11b59
Alex E Scott <aescott@outlook.com>
2017-08-01 16:32:11 -0700

    add lifestyle methods

--- Angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ Angular/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ViewEncapsulation,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,12 +18,51 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./server-element.component.css'],
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   @Input('srvElement') element: {type: string, name: string, content: string};
+  @Input() name: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log('constructor called!')
   }
 
+  ngAfterViewChecked() {
+    console.log('AfterViewChecked called!')
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!')
+  }
+
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked called!')
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!')
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!')
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit called!')
+  }
+
+  ngOnDestroy(){
+    console.log('ngOnDestroy called!')
+  }
 }
