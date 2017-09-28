2f00a5b9f687532ff09306a0e5b9dee6f6e51063
Alex E Scott <aescott@outlook.com>
2017-09-28 11:45:46 -0700

    add options to budget managet

--- ChromeExtensions/budgetManager/popup.js
+++ ChromeExtensions/budgetManager/popup.js
@@ -1,7 +1,11 @@
 $(function(){
+  chrome.storage.sync.get(['total', 'limit'], function(budget) {
+    $('#total').text(budget.total);
+    $('#limit').text(budget.limit);
+  });
+
   $('#spendAmount').click(function() {
     chrome.storage.sync.get('total', function(budget){
-      console.log(budget.total);
       var newTotal = 0;
       if (budget.total) {
         newTotal += parseInt(budget.total);
