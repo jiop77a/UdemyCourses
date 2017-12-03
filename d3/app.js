babb654a803550e2965cfa4ffa0230512a93e140
Alex E Scott <aescott@outlook.com>
2017-12-03 13:48:40 -0800

    initial binding

--- d3/app.js
+++ d3/app.js
@@ -1,2 +1,17 @@
-var el = d3.select('body').append('p')
-console.log(el)
+var dataset = [10, 20, 30, 40, 50];
+
+
+
+var el = d3.select('body')
+          .selectAll('p')
+          .data(dataset)
+          .enter()
+          .append('p')
+          .text("Hello World");
+            // .append('p')
+            // .classed('foo', true)
+            // .classed('bar', true)
+            // .attr( 'class', 'foo' )
+            // .text('hello world')
+            // .style('color', 'blue');
+console.log(el);
