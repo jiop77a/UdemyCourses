df5ae0817dd6af075da5426277260d4729855aad
Alex E Scott <aescott@outlook.com>
2017-12-04 13:08:48 -0800

    labels on chart

--- d3/section 4/app.js
+++ d3/section 4/app.js
@@ -1,9 +1,10 @@
 /* globals d3 */
 let chart_width = 800;
 let chart_height = 400;
+let bar_padding = 5;
 let data = [];
 
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 5; i++) {
   let num = Math.floor(Math.random() * 50);
   data.push(num);
 }
@@ -19,10 +20,23 @@ const generate = (data) => {
     .data(data)
     .enter()
     .append('rect')
-    .attr('x', (d, i) => i * (800 / data.length)) // 0 - 0, 1 - 30, 2, 60
-    .attr('y', 0)
-    .attr('width', 25)
-    .attr('height', 100);
+    .attr('x', (d, i) => i * (chart_width / data.length))
+    .attr('y', d => chart_height - d * 5)
+    .attr('width', chart_width / data.length - bar_padding)
+    .attr('height', d => d * 5)
+    .attr('fill', '#7ED26D');
+
+    //create labels
+    svg.selectAll('text')
+      .data(data)
+      .enter()
+      .append('text')
+      .text(d => d)
+      .attr('x', (d, i) => i * (chart_width / data.length) + (chart_width / data.length - bar_padding) / 2)
+      .attr('y', d => chart_height - d * 5 + 15)
+      .attr('font-size', '14')
+      .attr('fill', '#fff')
+      .attr('text-anchor', 'middle');
 
 };
 
