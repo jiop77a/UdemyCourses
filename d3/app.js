51553c526415babf7600ee5f07347b8b2f552e14
Alex E Scott <aescott@outlook.com>
2017-12-04 10:53:10 -0800

    d3 from csv

--- d3/app.js
+++ d3/app.js
@@ -1,18 +1,37 @@
-var dataset = [10, 20, 30, 40, 50];
+d3.csv('data.csv', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+  // generate(data.columns);
+});
 
+// const generate = (dataset) => {
 
+  // var dataset = [10, 20, 30, 40, 50];
 
-var el = d3.select('body')
-          .selectAll('p')
-          .data(dataset)
-          .enter()
-          .append('p')
-          .text((d) =>
-            `This paragraph is bound to the number ${d}`);
-            // .append('p')
-            // .classed('foo', true)
-            // .classed('bar', true)
-            // .attr( 'class', 'foo' )
-            // .text('hello world')
-            // .style('color', 'blue');
-console.log(el);
+  var el = d3.select('body')
+  .selectAll('p')
+  .data(dataset)
+  .enter()
+  .append('p')
+  .text((d) => `This paragraph is bound to the number ${d}`)
+  .attr( 'class', (d) => {
+    if (d > 25) {
+      return 'foo';
+    } else {
+      return null;
+    }
+  })
+  .classed('bar', d => d < 25)
+  // .text('hello world')
+  .style('color', (d) => {
+    if (d > 25) {
+      return 'red';
+    } else {
+      return 'blue';
+    }
+  });
+  console.log(el);
+
+}
