c3d81a0b3c52c277af4e7fb1645107569ad330b0
Alex E Scott <aescott@outlook.com>
2017-12-04 16:58:01 -0800

    time scales

--- d3/section_4/scatterplot/app.js
+++ d3/section_4/scatterplot/app.js
@@ -1,17 +1,36 @@
-let data            =   [
-    [ 400, 200 ],
-    [ 210,140 ],
-    [ 722,300 ],
-    [ 70,160 ],
-    [ 250,50 ],
-    [ 110,280 ],
-    [ 699,225 ],
-    [ 90, 220 ]
+// let data            =   [
+//     [ 400, 200 ],
+//     [ 210,140 ],
+//     [ 722,300 ],
+//     [ 70,160 ],
+//     [ 250,50 ],
+//     [ 110,280 ],
+//     [ 699,225 ],
+//     [ 90, 220 ]
+// ];
+let data = [
+  { date: '07/01/2017', num: 20},
+  { date: '07/02/2017', num: 37},
+  { date: '07/03/2017', num: 25},
+  { date: '07/04/2017', num: 45},
+  { date: '07/05/2017', num: 23},
+  { date: '07/06/2017', num: 33},
+  { date: '07/07/2017', num: 49},
+  { date: '07/08/2017', num: 40},
+  { date: '07/09/2017', num: 36},
+  { date: '07/10/2017', num: 27}
 ];
 
+let time_parse = d3.timeParse('%m/%d/%Y');
+let time_format = d3.timeFormat('%b %e');
 let chart_width     =   800;
 let chart_height    =   400;
 let padding = 50;
+
+//loop through dates
+data.forEach((e, i) => {
+  data[i].date = time_parse(e.date);
+})
 
 //svg
 let svg = d3.select("#chart")
@@ -21,17 +40,21 @@ let svg = d3.select("#chart")
 
 //scales
 
-let x_scale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d[0])])
+let x_scale = d3.scaleTime()
+        .domain([d3.min(data, d => d.date), d3.max(data, d => d.date)])
         .range([padding, chart_width - padding * 2]);
 
 let y_scale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d[1])])
+        .domain([0, d3.max(data, d => d.num)])
         .range([chart_height - padding, padding]);
 
-let r_scale = d3.scaleLinear()
-        .domain([0, d3.max(data, d => d[1])])
-        .range([5, 30]);
+// let r_scale = d3.scaleLinear()
+//         .domain([0, d3.max(data, d => d[1])])
+//         .range([5, 30]);
+
+let a_scale = d3.scaleSqrt()
+        .domain([0, d3.max(data, d => d.num)])
+        .range([0, 25]);
 
 
 //circles
@@ -39,9 +62,9 @@ svg.selectAll('circle')
   .data(data)
   .enter()
   .append('circle')
-  .attr('cx', d => x_scale(d[0]))
-  .attr('cy', d => y_scale(d[1]))
-  .attr('r', d => d[1] / 10)
+  .attr('cx', d => x_scale(d.date))
+  .attr('cy', d => y_scale(d.num))
+  .attr('r', d => a_scale(d.num))
   .attr('fill', '#D1AB0E');
 
 //labels
@@ -49,6 +72,6 @@ svg.selectAll('text')
   .data(data)
   .enter()
   .append('text')
-  .text(d => d.join(','))
-  .attr('x', d => x_scale(d[0]))
-  .attr('y', d => y_scale(d[1]));
+  .text(d => time_format(d.date))
+  .attr('x', d => x_scale(d.date))
+  .attr('y', d => y_scale(d.num));
