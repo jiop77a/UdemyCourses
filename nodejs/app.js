823c8b4bbae3c90eddd421d4351d880079293fd4
Alex E Scott <aescott@outlook.com>
2018-02-14 14:08:12 -0800

    modules

---
+++ nodejs/app.js
@@ -0,0 +1,15 @@
+// var arr = [];
+//
+// for (let i = 0; i < 4; i++) {
+//   console.log(arr);
+//   arr.push(function() {
+//     return (`${i} is the best number`);
+//   });
+// }
+//
+// console.log(arr[0]());
+// console.log(arr[1]());
+// console.log(arr[2]());
+// console.log(arr[3]());
+
+require('./greet.js');
