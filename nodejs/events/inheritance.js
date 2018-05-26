c273ba5a1a3f54f633bdfe2ac6d6253775000286
Alex E Scott <aescott@outlook.com>
2018-05-25 17:58:59 -0700

    inheriting from the event emitter

---
+++ nodejs/events/inheritance.js
@@ -0,0 +1,38 @@
+//prototypes
+
+// let person = {
+//   firstname: 'a',
+//   lastname: 'b',
+//   greet: function() {
+//     return this.firstname + ' ' + this.lastname;
+//   }
+// };
+//
+// let john = Object.create(person);
+// john.firstname = 'John';
+// john.lastname = 'Smith';
+//
+// console.log(john);
+
+let EventEmitter = require('events');
+let util = require('util');
+
+class Greetr {
+  constructor() {
+    this.greeting = 'Hello world!';
+  }
+
+}
+
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data) {
+  console.log(this.greeting + ': ' + data);
+  this.emit('greet', data);
+};
+
+let greeter1 = new Greetr();
+
+greeter1.on('greet', (data) => console.log('Someone greeted!: ' + data));
+
+greeter1.greet("Tony");
